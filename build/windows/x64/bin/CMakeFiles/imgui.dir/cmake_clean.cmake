c849b83d32152b639c8073f520a858f03b831f90
BabyYod4 <razzlenator@protonmail.com>
2020-05-19 16:53:12 +0200

    Trying to keep folder structure but remove files of build folder

---
+++ build/windows/x64/bin/CMakeFiles/imgui.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../../../lib/windows/x64/libimgui.a"
+  "../../../../lib/windows/x64/libimgui.pdb"
+  "CMakeFiles/imgui.dir/__/lib/windows/x64/imgui/imgui.cpp.obj"
+  "CMakeFiles/imgui.dir/__/lib/windows/x64/imgui/imgui_demo.cpp.obj"
+  "CMakeFiles/imgui.dir/__/lib/windows/x64/imgui/imgui_draw.cpp.obj"
+  "CMakeFiles/imgui.dir/__/lib/windows/x64/imgui/imgui_impl_glfw_gl3.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/windows/x64/bin/CMakeFiles/twom.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/engine/LucidyEngine/scene/scene_controller.cpp" "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/build/windows/x64/bin/CMakeFiles/twom.dir/__/engine/LucidyEngine/scene/scene_controller.cpp.obj"
+  "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/engine/LucidyEngine/window/debug_window.cpp" "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/build/windows/x64/bin/CMakeFiles/twom.dir/__/engine/LucidyEngine/window/debug_window.cpp.obj"
+  "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/engine/LucidyEngine/window/normal_window.cpp" "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/build/windows/x64/bin/CMakeFiles/twom.dir/__/engine/LucidyEngine/window/normal_window.cpp.obj"
+  "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/src/main.cpp" "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/build/windows/x64/bin/CMakeFiles/twom.dir/main.cpp.obj"
+  "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/src/scenes/example_scene.cpp" "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/build/windows/x64/bin/CMakeFiles/twom.dir/scenes/example_scene.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../src"
+  "../../../include/windows/x64"
+  "../../../engine"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/build/windows/x64/bin/CMakeFiles/imgui.dir/DependInfo.cmake"
+  "C:/Development/Workspaces/OpenGL/the-wonders-of-mazalt/build/windows/x64/bin/CMakeFiles/stb.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
