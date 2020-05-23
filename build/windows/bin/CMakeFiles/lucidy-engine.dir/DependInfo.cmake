896daf14c816d143e8448b33ccc685ab66b7ff03
BabyYod4 <razzlenator@protonmail.com>
2020-05-23 17:42:07 +0200

    Fixed Imgui Static lib

---
+++ build/windows/bin/CMakeFiles/lucidy-engine.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Development/Workspace/OpenGL/lucidy-engine/engine/LucidyEngine/scene/scene_controller.cpp" "C:/Development/Workspace/OpenGL/lucidy-engine/build/windows/bin/CMakeFiles/lucidy-engine.dir/__/engine/LucidyEngine/scene/scene_controller.cpp.obj"
+  "C:/Development/Workspace/OpenGL/lucidy-engine/engine/LucidyEngine/window/gui_window.cpp" "C:/Development/Workspace/OpenGL/lucidy-engine/build/windows/bin/CMakeFiles/lucidy-engine.dir/__/engine/LucidyEngine/window/gui_window.cpp.obj"
+  "C:/Development/Workspace/OpenGL/lucidy-engine/engine/LucidyEngine/window/normal_window.cpp" "C:/Development/Workspace/OpenGL/lucidy-engine/build/windows/bin/CMakeFiles/lucidy-engine.dir/__/engine/LucidyEngine/window/normal_window.cpp.obj"
+  "C:/Development/Workspace/OpenGL/lucidy-engine/src/main.cpp" "C:/Development/Workspace/OpenGL/lucidy-engine/build/windows/bin/CMakeFiles/lucidy-engine.dir/main.cpp.obj"
+  "C:/Development/Workspace/OpenGL/lucidy-engine/src/scenes/example_scene.cpp" "C:/Development/Workspace/OpenGL/lucidy-engine/build/windows/bin/CMakeFiles/lucidy-engine.dir/scenes/example_scene.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../src"
+  "../../../include"
+  "../../../engine"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
